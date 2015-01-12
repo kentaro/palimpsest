dc01e918bdbc2817a0ccc958ccd84920b201cd5c
Kentaro Kuribayashi <kentarok@gmail.com>
2015-01-12 15:43:05 +0900

    リスト表示

--- app/src/main/java/org/kentarok/android/palimpsest/fragments/TaskListFragment.java
+++ app/src/main/java/org/kentarok/android/palimpsest/fragments/TaskListFragment.java
@@ -1,21 +1,19 @@
 package org.kentarok.android.palimpsest.fragments;
 
-import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import org.kentarok.android.palimpsest.R;
 import org.kentarok.android.palimpsest.models.Task;
 
-public class TaskListFragment extends Fragment implements AbsListView.OnItemClickListener {
-    private ListAdapter adapter;
+public class TaskListFragment extends ListFragment {
+    private ArrayAdapter<Task> adapter;
     private AbsListView listView;
 
     public static TaskListFragment newInstance() {
@@ -29,36 +27,31 @@ public class TaskListFragment extends Fragment implements AbsListView.OnItemClic
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        adapter = new ArrayAdapter<Task>(getActivity(),
-                android.R.layout.simple_list_item_1, android.R.id.text1, Task.taskList());
+        ArrayAdapter<Task> adapter = (new ArrayAdapter<Task>(getActivity(), R.layout.fragment_task_list, Task.taskList()){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                View view = inflater.inflate(R.layout.fragment_task_list_item, parent, false);
+
+                Task task = getItem(position);
+                TextView textTitle = (TextView)view.findViewById(R.id.task_list_item_title);
+                textTitle.setText(task.title);
+                TextView textCurrentCount = (TextView)view.findViewById(R.id.task_list_item_current_count);
+                textCurrentCount.setText(task.currentCount().toString());
+
+                return view;
+            }
+        });
+        setListAdapter(adapter);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_task_list, container, false);
-
-        listView = (AbsListView)view.findViewById(android.R.id.list);
-        ((AdapterView<ListAdapter>) listView).setAdapter(adapter);
-        listView.setOnItemClickListener(this);
-
         return view;
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-    }
-
-    /**
-     * The default content for this Fragment has a TextView that is shown when
-     * the list is empty. If you would like to change the text, call this method
-     * to supply the text it should use.
-     */
     public void setEmptyText(CharSequence emptyText) {
         View emptyView = listView.getEmptyView();
 
